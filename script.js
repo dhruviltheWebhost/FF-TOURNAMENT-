d79fe194ed35cf96c31af7996d3845cb027be216
dhruviltheWebhost <barotdhruvilc@gmail.com>
2024-11-08 16:09:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add any interactive elements here, such as animations or dynamic content
+window.onload = function() {
+    console.log("Welcome to the Free Fire Squad Tournament page!");
+};
